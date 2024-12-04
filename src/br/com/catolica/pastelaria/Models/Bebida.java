9bd6f84a92d83b3ecc8e5708d6d3739f6e933a0e
Zep213 <jososephbem100@gmail.com>
2024-12-04 11:33:22 -0300

    pastelariaPOO

--- src/br/com/catolica/pastelaria/Models/Bebida.java
+++ src/br/com/catolica/pastelaria/Models/Bebida.java
@@ -1,4 +1,60 @@
 package br.com.catolica.pastelaria.Models;
 
-public class Bebida {
+import br.com.catolica.pastelaria.Enums.CategoriaProduto;
+
+public class Bebida extends Produtos{
+    private int mls;
+    private double preco;
+    private boolean gelo;
+    private boolean limao;
+    private String sabor;
+
+    public Bebida(String nome, CategoriaProduto categoria, int mls, double preco, boolean gelo, boolean limao, String sabor) {
+        super(nome, categoria);
+        this.mls = mls;
+        this.preco = preco;
+        this.gelo = gelo;
+        this.limao = limao;
+        this.sabor = sabor;
+    }
+
+    public int getMls() {
+        return mls;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public boolean isGelo() {
+        return gelo;
+    }
+
+    public boolean isLimao() {
+        return limao;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setGelo(boolean gelo) {
+        this.gelo = gelo;
+    }
+
+    public void setLimao(boolean limao) {
+        this.limao = limao;
+    }
+
+    @Override
+    public String toString() {
+        return "Bebida{" +
+                "categoria: " + categoria +
+                ", nome: '" + nome + '\'' +
+                ", limao: " + limao +
+                ", gelo: " + gelo +
+                ", preco: " + preco +"R$"+
+                ", mls: " + mls +
+                '}';
+    }
 }
--- src/br/com/catolica/pastelaria/Models/Pastel.java
+++ src/br/com/catolica/pastelaria/Models/Pastel.java
@@ -1,24 +1,29 @@
 package br.com.catolica.pastelaria.Models;
 
 import br.com.catolica.pastelaria.Enums.CategoriaProduto;
+import br.com.catolica.pastelaria.Exeptions.EntradaInvalidaException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Pastel extends Produtos{
 
-    int quantidade;
-    List<String> recheios;
-    double peso;
-    double preco;
+    private int quantidade;
+    private List<String> recheios;
+    private double peso;
+    private double preco;
 
-    public Pastel(String nome, CategoriaProduto categoria, double peso, List<String> recheios,double preco) {
+    public Pastel(String nome, CategoriaProduto categoria, double peso, List<String> recheios,double preco, int quantidade) {
         super(nome, categoria);
         this.peso = peso;
-        this.recheios = recheios;
         this.preco = preco;
+        this.recheios = recheios != null ? new ArrayList<>(recheios) : new ArrayList<>();
+        this.quantidade = quantidade;
     }
 
-    public List<String> getRecheios() {
+
+    public List<String> getRecheios(){
         return recheios;
     }
 
@@ -34,19 +39,32 @@ public class Pastel extends Produtos{
         this.quantidade = quantidade;
     }
     public void adicionarRecheio(String recheio) {
-        if (recheio != null && !recheio.isEmpty()) {
+        if (recheio != null && !recheio.trim().isEmpty()){
             recheios.add(recheio);
-        } else {
-            throw new IllegalArgumentException("Recheio inválido.");
+            System.out.println("Recheio '" + recheio + "' adicionado com sucesso.");
+        }else {
+            throw new EntradaInvalidaException("Recheio invalido");
         }
     }
     public void removerRecheio(String recheio) {
-        recheios.remove(recheio);
+        if (recheios.contains(recheio)) {
+            recheios.remove(recheio);
+            System.out.println("Recheio removido com sucesso");
+        } else {
+            System.out.println("Recheio não encontrado.");
+        }
     }
-    public void exibirDetalhes() {
-        System.out.println("Pastel: " + nome);
-        System.out.println("Preço: R$" + preco);
-        System.out.println("Recheios: " + String.join(", ", recheios));
-        System.out.println("Peso: " + peso + "g");
+
+    @Override
+    public String toString() {
+        return "Pastel{" +
+                "categoria=" + categoria +
+                ", nome='" + nome + '\'' +
+                ", preco: " + preco + "R$" +
+                ", peso: " + peso +
+                ", recheios: " + recheios +
+                ", quantidade: " + quantidade +
+                '}';
     }
+
 }
